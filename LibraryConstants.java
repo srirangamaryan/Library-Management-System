347fc3c839002552b26c188c477f7ce284f28e08
Aryan Srirangam <srirangamaryan@gmail.com>
2026-04-17 17:16:35 +0530

    Create LibraryConstants.java

---
+++ LibraryConstants.java
@@ -0,0 +1,8 @@
+package CWH.Library_Management;
+
+public class LibraryConstants {
+    public static final String WELCOME_MSG = "Welcome to the Library Management System";
+    public static final String EXIT_MSG = "Thank you for using the Library. Goodbye!";
+    public static final int MAX_BOOKS_PER_USER = 5;
+    public static final String DATE_FORMAT = "dd-MM-yyyy";
+}
